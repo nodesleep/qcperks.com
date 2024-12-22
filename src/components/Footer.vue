cf17d5cb86bd2354459a6db9d171e7ded7bf69d0
Aron Sweet <sweet@andrews.edu>
2024-12-22 14:01:19 -0600

    CSS updates for Mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@ const year = date.getFullYear();
 </script>
 
 <template>
-    <footer class="cf w-100 ph5-ns center">
+    <footer class="cf w-100 ph3 ph5-ns center">
         <div class="db dt-ns mw9 center w-100">
             <div class="fl">
                 <p class="f6 db lh-solid">&copy; {{ year }} info@qcperks.com</p>
